ddcc14ca738e03ef5695c4e7b8886cdfb9c480ba
Kilanov <sam.kilanoff@gmail.com>
2024-04-07 13:34:24 +0500

    add simple AnswerCsvConverter test

---
+++ spring-01-hw/src/test/java/ru/skilanov/spring/dao/dto/AnswerCsvConverterTest.java
@@ -0,0 +1,25 @@
+package ru.skilanov.spring.dao.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.skilanov.spring.domain.Answer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AnswerCsvConverterTest {
+
+    private AnswerCsvConverter answerCsvConverter;
+
+    @BeforeEach
+    public void setUp() {
+        answerCsvConverter = new AnswerCsvConverter();
+    }
+
+    @Test
+    public void whenConvertAnswerThenItConverted() {
+        Answer answer = (Answer) answerCsvConverter.convertToRead("It is right answer!%true");
+        assertEquals("It is right answer", answer.text());
+        assertTrue(answer.isCorrect());
+    }
+}
